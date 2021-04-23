ff0bd6ede03e352f759ec9683891917035674b77
Wurdsmith <wurdsmithing@gmail.com>
2021-04-23 16:16:45 -0500

    commit tree cde948d3f353a438a3707cb15006ac9609d1cb37

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 5;
+const num4 = 24;
+const mod = num4 % num3;
+const numbers = (2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+const max = Math.max(numbers);
